256a28919b1916658c8fdcfcf0ebb08b541381c4
anyel-ec <paulangel826@gmail.com>
2024-07-02 23:27:51 -0500

    update docker-compose to connected

--- backend/main.js
+++ backend/main.js
@@ -3,20 +3,19 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  const result = await prisma.product.create({
-    data: {
-      description: "Zapatos blancos",
-      name: "Zapatos Lebron",
-    },
-  });
-
-  console.log(result);
+  try {
+    const result = await prisma.product.create({
+      data: {
+        description: "Zapatos blancos",
+        name: "Zapatos Lebron",
+      },
+    });
+    console.log(result);
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await prisma.$disconnect();
+  }
 }
 
-main()
-  .catch((e) => {
-    throw e;
-  })
-  .finally(async () => {
-    await prisma.$disconnect();
-  });
+main();
